8a1b9222a1da9c548a54853cc66e5c8d47970edc
amit nakhole <amit.nakhole@yahoo.com>
2025-07-04 05:53:44 +0000

    feat:Testing done

---
+++ models/intermediate/customer_orders.sql
@@ -0,0 +1,12 @@
+with
+    customer_orders as (
+        select  c.customer_id, 
+                sum(o.total_price) as price, 
+                max(o.order_date) date
+        from {{ ref("stg_customers") }} as c
+        join {{ ref("stg_orders") }} as o on c.customer_id = o.customer_id
+        where c.customer_id <= 1000
+        group by 1
+    )
+select *
+from customer_orders
---
+++ models/stage/stg_nations.sql
@@ -0,0 +1,13 @@
+{{ config(materialized='view') }}
+
+with
+    nation as (
+        select
+            n_nationkey as nation_id,
+            n_name as name,
+            n_regionkey as region_id,
+            n_comment as comment
+        from {{ source("src","nations")}}
+    )
+select *
+from nation
